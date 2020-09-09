db24dcb2c5a4155f7c7e43336b90ca594ac3271b
Lucas L <lucas.lopesdf.gmail.com>
2020-09-09 12:13:39 -0300

    Implementação Share

--- lib/pages/carros/carros_listview.dart
+++ lib/pages/carros/carros_listview.dart
@@ -3,6 +3,7 @@ import 'package:carros/model/carro.dart';
 import 'package:carros/pages/carros/carro_page.dart';
 import 'package:carros/util/nav.dart';
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class CarrosListView extends StatelessWidget {
   final List<Carro> carros;
@@ -18,60 +19,62 @@ class CarrosListView extends StatelessWidget {
         itemBuilder: (context, index) {
           Carro c = carros[index];
           return Container(
-              height: 280,
-              child: InkWell(
-                  onTap: () {
-                    _onClickCarro(context, c);
-                  },
-                  onLongPress: () {
-                    _onLongClickCarro (context, c);
-                  },
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Center(
-                            child: CachedNetworkImage(
-                              imageUrl: c.urlFoto ??
-                                  "http://www.livroandroid.com.br/livro/carros/classicos/Dodge_Challenger.png",
-                              width: 250,
-                            ),
-                          ),
-                          Text(
-                            c.nome,
-                            style: TextStyle(fontSize: 25),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Text(
-                            "Descrição",
-                            style: TextStyle(fontSize: 16),
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          ButtonBarTheme(
-                            data: ButtonBarTheme.of(context),
-                            child: ButtonBar(
-                              children: <Widget>[
-                                FlatButton(
-                                  child: const Text('DETALHES'),
-                                  onPressed: () => _onClickCarro(context, c),
-                                ),
-                                FlatButton(
-                                  child: const Text('SHARE'),
-                                  onPressed: () {
-                                    _onClickShare(context, c);
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+            height: 280,
+            child: InkWell(
+              onTap: () {
+                _onClickCarro(context, c);
+              },
+              onLongPress: () {
+                _onLongClickCarro(context, c);
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Center(
+                        child: CachedNetworkImage(
+                          imageUrl: c.urlFoto ??
+                              "http://www.livroandroid.com.br/livro/carros/classicos/Dodge_Challenger.png",
+                          width: 250,
+                        ),
                       ),
-                    ),
-                  )));
+                      Text(
+                        c.nome,
+                        style: TextStyle(fontSize: 25),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        "Descrição",
+                        style: TextStyle(fontSize: 16),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      ButtonBarTheme(
+                        data: ButtonBarTheme.of(context),
+                        child: ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                              child: const Text('DETALHES'),
+                              onPressed: () => _onClickCarro(context, c),
+                            ),
+                            FlatButton(
+                              child: const Text('SHARE'),
+                              onPressed: () {
+                                _onClickShare(context, c);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
         },
       ),
     );
@@ -82,32 +85,47 @@ class CarrosListView extends StatelessWidget {
   }
 
   void _onLongClickCarro(BuildContext context, Carro c) {
-    showDialog(context: context, builder: (context) {
-      return SimpleDialog (
-        title: Text (c.nome),
-        children: <Widget>[
-          ListTile (
-            title: Text("Detalhes"),
-            leading: Icon(Icons.directions_car),
-            onTap: () {
-              pop(context);
-              _onClickCarro(context, c);
-            },
-          ),
-          ListTile (
-            title: Text("Share"),
-            leading: Icon(Icons.share),
-            onTap: () {
-              pop(context);
-              _onClickShare(context, c);
-            },
-          )
-        ],
-      );
-    });
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  c.nome,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text("Detalhes"),
+                leading: Icon(Icons.directions_car),
+                onTap: () {
+                  pop(context);
+                  _onClickCarro(context, c);
+                },
+              ),
+              ListTile(
+                title: Text("Share"),
+                leading: Icon(Icons.share),
+                onTap: () {
+                  pop(context);
+                  _onClickShare(context, c);
+                },
+              )
+            ],
+          );
+        });
   }
 
   void _onClickShare(BuildContext context, Carro c) {
     print("Share ${c.nome}");
+
+    Share.share(c.urlFoto);
+
   }
 }
